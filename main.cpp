56e042bfe8808a205aeda86fa71458038bc38445
h2ckff <h2ckff@gmail.com>
2020-03-25 17:43:53 +0100

    I added loop in main and added all needed strings etc.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 include <iostream>
 namespace std;
 
+string fin = "fin";
+string temp;
+string tempcin
+
 int main()
 {
-    cout<<"Uff"<<endl;
-    return 0;
+    	cout<<"SC2trainting 0.0.1"<<endl;
+    	cout<<"Type START to run program. Type FIN to stop time."<<endl;
+	while(tempcin!=fin)
+	{
+		
+	}
+	
+   	return 0;
 }
