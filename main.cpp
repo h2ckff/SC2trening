b473b2db83d12c25b9f0571cc7f947c43f1f781f
h2ckff <h2ckff@gmail.com>
2020-03-25 18:07:28 +0100

    troche niedziala ale ok

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
-include <iostream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
 namespace std;
 
 string fin = "fin";
-string temp;
-string tempcin
+string temp = "";
+string tempcin = "";
+
+string randomChar()
+{
+    int i  = rand()%25;
+    if(i<7)
+    {
+        string tempa = to_string(i);
+        return  tempa;
+    }
+    else
+    {
+        char n = i;
+        return n;
+    }
+}
 
 int main()
 {
@@ -11,8 +28,8 @@ int main()
     	cout<<"Type START to run program. Type FIN to stop time."<<endl;
 	while(tempcin!=fin)
 	{
-		
+
 	}
-	
+
    	return 0;
 }
