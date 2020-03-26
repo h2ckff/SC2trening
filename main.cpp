7e800b2447281ef38a1968130ee203a92d621086
h2ckff <h2ckff@gmail.com>
2020-03-26 14:21:42 +0100

    Added generator function and made main with all needed parts to make it work

--- main.cpp
+++ main.cpp
@@ -4,7 +4,37 @@
 #include <cstdlib>
 using namespace std;
 
+string input = "";
+string expected = "";
+int points = 0;
+
+void generator()
+{
+  char rnd = 'a' + rand()%24;
+  expected = rnd;
+  cout<<expected<<endl;
+}
+
 int main()
 {
   cout<<"Hellow world";
+
+  while(input!="fin")
+  {
+    generator();
+    cin>>input;
+
+    if(input==expected)
+    {
+      points++;
+      cout<<"Good job";
+    }
+    else
+    {
+      cout<<"You missed";
+    }
+
+    cout<<" | Your score: "<<points<<endl;
+  }
+
 }
