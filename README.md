248c0ad5746cdb2823dfb49082f6afc8ef21e834
h2ckff <h2ckff@gmail.com>
2020-03-26 13:33:55 +0100

    Removed all the shity codeand added more specific points to readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SC2trening
-Application made to help increase your APM in Stracraft 2.
+Application made to help increase your APM in Starcraft 2.
 
 Things to do:
-	1. Make a program
+	1. Make randomchar generator
+	2. Make main function to make it somehow work
+	3. Add fetures
--- main.cpp
+++ main.cpp
@@ -1,35 +1,10 @@
 #include <iostream>
 #include <string>
+#include <random>
 #include <cstdlib>
 namespace std;
 
-string fin = "fin";
-string temp = "";
-string tempcin = "";
-
-string randomChar()
-{
-    int i  = rand()%25;
-    if(i<7)
-    {
-        string tempa = to_string(i);
-        return  tempa;
-    }
-    else
-    {
-        char n = i;
-        return n;
-    }
-}
-
 int main()
 {
-    	cout<<"SC2trainting 0.0.1"<<endl;
-    	cout<<"Type START to run program. Type FIN to stop time."<<endl;
-	while(tempcin!=fin)
-	{
-
-	}
-
-   	return 0;
+  cout<<"Hellow world";
 }
