5cddf299b5a3e3dfd8ab213c3a08b8e833ffffaf
h2ckff <h2ckff@gmail.com>
2020-03-26 17:57:04 +0100

    Changed program to clear terminal each time you make acction. Also made everything work as it should finaly:)

--- main.cpp
+++ main.cpp
@@ -10,45 +10,69 @@ string input = "";
 string expected = "";
 int points = 0;
 char a;
+char tempRan;
+string tempStr;
+bool score=0;
 
 //Function that generates the character to type
-void generator()
+string generator()
 {
-  char rnd = 'a' + rand()%24;
-  expected = rnd;
-  cout<<expected<<endl;
+  int i = rand()%36;
+  if(i<10)
+  {
+    return to_string(i);
+  }
+  else
+  {
+    tempRan = 'a' + (i - 10);
+    tempStr = tempRan;
+    return tempStr;
+  }
 }
 
 //Main function where takes place most of things
 int main()
 {
   //First text that appers right after compilaton
-  cout<<"SC2traing v.0.1"<<endl<<"To stop enter ?"<<endl;
+  cout<<"SC2traing v.1.0"<<endl<<"To stop enter ?"<<endl;
   system("pause");
   cout<<endl;
 
 //Main loop where takes place somethings
   while(input!="?")
   {
-    generator();
-    a = getch();
-    input = a;
+    cout<<"SC2traing v.1.0"<<endl<<"To stop enter ?"<<endl<<endl;
 
-    if(input==expected)
+    if(score == 1)
     {
-      points++;
       cout<<"Good job";
     }
     else
     {
       cout<<"You missed";
     }
-
     cout<<" | Your score: "<<points<<endl<<endl;
+
+    expected = generator();
+    cout<<"\t"<<expected<<endl;
+    a = getch();
+    input = a;
+
+    if(input==expected)
+    {
+      points++;
+      score = 1;
+    }
+    else
+    {
+      score = 0;
+    }
+    system("cls");
   }
 
   //Ending information and system("pause"), to freeze app
-  cout<<endl<<endl<<"You're done. Your score is "<<points<<endl;
+  cout<<"SC2traing v.1.0"<<endl<<"To stop enter ?"<<endl<<endl;
+  cout<<"You're done. Your score is equal "<<points<<endl;
   system("pause");
   return 0;
 }
